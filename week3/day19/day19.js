ff2b76bd1393746c07fef6f49385abb6bf76f4bf
Jacksparrow7-coder <sugishiva0@gmail.com>
2026-06-14 18:56:57 +0530

    day21 done

---
+++ week3/day19/day19.js
@@ -0,0 +1,21 @@
+async function getGoogleInfo() {
+    try{ 
+     let res = await fetch("https:/api.github.com/users/google");
+//   console.log(res);
+  let data = await res.json()
+  console.log(data);
+//   console.log(data.name);
+}
+  catch(error){
+    console.log("something went wrong :", error);
+  }
+}
+getGoogleInfo()
+
+
+// Promises style:          async/await style:
+// ──────────────           ─────────────────
+// fetch()                  async function
+//   .then(res => ...)        try { await fetch() }
+//   .then(data => ...)       catch(error) { }
+//   .catch(error => ...)
